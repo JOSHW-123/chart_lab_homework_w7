35b8d60862fee9ebb8a90dd352b7469e70443ec2
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-28 19:20:42 +0100

    First Commit

---
+++ src/containers/ChartContainer.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from 'react';
+// Import other components & css later.
+
+const ChartContainer = () => {
+    const [charts, setCharts] = useState([]);
+    const [selectedChart, setSelectedChart] = useState(null);
+}
